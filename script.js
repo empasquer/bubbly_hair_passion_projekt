d6964dd864d7c7f153eaac339731c98b7ea22bb8
emma673r <90177139+emma673r@users.noreply.github.com>
2022-02-19 22:10:21 +0100

    start sortering men cors fejl

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
 // burger menu
-
 
 const menu = document.querySelector(".burger_menu");
 const menuItems = document.querySelectorAll("li");
@@ -25,17 +23,63 @@ hamburger.addEventListener("click", toggleMenu);
 menuItems.forEach(function (li) {
   li.addEventListener("click", toggleMenu);
 });
+
 // burger menu slut
+
+// første pop up
 const popup1 = document.querySelector("#popup_opening");
 const luk1 = document.querySelector("#luk1");
 
+// sortering af hårtyper
+const url = "https://shampoobizz-80fc.restdb.io/rest/produkt";
+const key = "91a1a859ee65aa0922db45a35f046a0900eec";
+const options = {
+  headers: {
+    "x-apikey": key,
+  },
+};
+
+const btn = document.querySelectorAll("button");
+const container = document.querySelector("#liste");
+const temp = document.querySelector("template");
+const h3 = document.querySelector("#kategorih3");
+
+const popup = document.querySelector("#popup");
+const luk = document.querySelector("#luk");
+
+// variabel med hvad jeg vil sorter
+
+let hairType;
+let filter = "alle";
+
 window.addEventListener("DOMContentLoaded", start);
 
-
 function start() {
-luk1.addEventListener("click", lukOpeningPopup);
+  luk1.addEventListener("click", lukOpeningPopup);
+  btn.forEach((knap) => knap.addEventListener("click", filterHairType));
+  hentData();
 }
 
 function lukOpeningPopup() {
-    popup1.classList = "noshow";
+  popup1.classList = "noshow";
+}
+
+function filterHairType() {
+  filter = this.dataset.kategori;
+  document.querySelector(".valgt").classList.remove("valgt");
+  this.classList.add("valgt");
+
+  visHairType();
+  h3.textContent = this.textContent;
+}
+
+async function hentData() {
+  const respons = await fetch(url, options);
+  hairType = await respons.json();
+  console.log(hairType);
+  visHairType();
+}
+
+function visHairType() {
+  console.log(visHairType);
 }
